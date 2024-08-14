186e206be3abb1c88339fbcb104702b9463b94db
Polina <veshapoli@gmail.com>
2024-08-14 12:03:20 +0300

    add new search-filter

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -3,11 +3,14 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import { useState, useEffect } from "react";
-
 function App() {
   const [cartOpened, setCartOpened] = useState(false);
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const onChangeSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
   useEffect(() => {
     fetch("https://66b494489f9169621ea39447.mockapi.io/items")
       .then((res) => {
@@ -46,15 +49,30 @@ function App() {
       <Header onClickCart={() => setCartOpened(1)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу: "${searchValue}"`
+              : "Все кроссовки"}
+          </h1>
           <div className="search-block">
             <img width={14} height={14} src="/img/search.svg" />
-            <input placeholder="Поиск..." />
+            <input
+              value={searchValue}
+              onChange={onChangeSearch}
+              placeholder="Поиск..."
+            />
+            <img
+              onClick={() =>setSearchValue('')}
+              className="clear cu-p"
+              src="/img/btn-delete.svg"
+              alt="Remove"
+            />
           </div>
         </div>
         <div className=" items d-flex flex-wrap">
-          {items.map((value) => (
+          {items.filter(value => value.title.toLowerCase().includes(searchValue.toLowerCase())).map((value) => (
             <Card
+              key={value.title}
               title={value.title}
               imageUrl={value.imageUrl}
               price={value.price}
